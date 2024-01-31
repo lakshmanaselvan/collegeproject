c4a7a4b4efcfacaab5cc82d4c9d20f2615192d97
lakshmanaselvan <lakshmanaselvan252@gmail.com>
2024-01-31 09:06:58 +0530

    first commit

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('',views.home,name="home"),
-    path('events_list',views.event_list, name="event")
+    path('events_list',views.event_list, name="event"),
+    path('add_events',views.add_events,name="add_events"),
+    path('generate_pdf',views.generate_pdf,name="generate_pdf")
 ]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,35 @@
 from django.shortcuts import render
 from .models import Event
+from django.http import FileResponse
+import io
+from reportlab.pdfgen import canvas
+from reportlab.lib.units import inch
+from reportlab.lib.pagesizes import letter
+from .models import Venue
+
+# Generate Event  PDF
+def generate_pdf(request):
+    buf = io.BytesIO()
+    c = canvas.Canvas(buf, pagesize=letter, bottomup=0)
+    textob = c.beginText()
+    textob.setTextOrigin(inch, inch)
+    textob.setFont('Helvetica', 14)
+    events = Event.objects.all()
+    lines = []
+    for event in events:
+        lines.append(event.title)
+        lines.append(event.venue)
+        lines.append(event.organizer)
+        lines.append(" ")
+
+    for line in lines:
+        textob.textLine(line)
+    
+    c.drawText(textob)
+    c.showPage()
+    c.save()
+    buf.seek(0)
+    return FileResponse(buf, as_attachment=True, filename='Events.pdf')
 
 def home(request):
     return render(request, "home.html", {})
@@ -7,3 +37,6 @@ def home(request):
 def event_list(request):
     events = Event.objects.all()
     return render(request, "events.html", {'events':events})
+
+def add_events(request):
+    return render(request, "add_events.html", {})
